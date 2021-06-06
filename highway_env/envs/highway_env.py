f4694725b5295489717816a5b25514928ac30048
Nesko <cnesko@gmail.com>
2021-06-06 15:02:20 +0200

    modified _reward function
    def _reward(self, action: Action) -> float:
            """
            The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
            :param action: the last action performed
            :return: the corresponding reward
            """
            neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
            lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
                else self.vehicle.lane_index[2]
            lane_change = action == 0 or action == 2
            scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
            reward = \
                + self.config["collision_reward"] * self.vehicle.crashed \
                + self.config["lane_change_reward"] * lane_change \
                + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
            reward = utils.lmap(reward,
                              [self.config["collision_reward"] + self.config["lane_change_reward"],
                               self.config["high_speed_reward"]],
                              [0, 1])
            reward = 0 if not self.vehicle.on_road else reward
            return reward

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -84,14 +84,15 @@ class HighwayEnv(AbstractEnv):
         neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
         lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
             else self.vehicle.lane_index[2]
+        lane_change = action == 0 or action == 2
         scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
         reward = \
             + self.config["collision_reward"] * self.vehicle.crashed \
-            + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
+            + self.config["lane_change_reward"] * lane_change \
             + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
         reward = utils.lmap(reward,
-                          [self.config["collision_reward"],
-                           self.config["high_speed_reward"] + self.config["right_lane_reward"]],
+                          [self.config["collision_reward"] + self.config["lane_change_reward"],
+                           self.config["high_speed_reward"]],
                           [0, 1])
         reward = 0 if not self.vehicle.on_road else reward
         return reward
